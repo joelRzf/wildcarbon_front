54b01697975620eac10178a62b160ddf4dc7bc50
Joel Razafi <raza.joel.dev@gmail.com>
2023-06-09 19:45:06 +0200

    remove useless vars

--- src/pages/CreateActivityPage.tsx
+++ src/pages/CreateActivityPage.tsx
@@ -30,70 +30,7 @@ import CircularProgress from '@mui/material/CircularProgress'
 
 type TUnit = 'gr' | 'kg'
 
-enum activityTypeName {
-  transport = 'transport',
-  numeric = 'numeric',
-  food = 'food',
-  energy = 'energy',
-  appliance = 'appliance',
-  other = 'other',
-  all = 'all',
-}
 
-enum activityTypeLabel {
-  Transport = 'Transport',
-  Numerique = 'Numérique',
-  Alimentation = 'Alimentation',
-  Energie = 'Energie',
-  Electromenager = 'Electroménager',
-  Autre = 'Autre',
-  QuantiteKG = 'Quantité (kg)',
-}
-
-const activityTypesList = [
-  {
-    activityTypeId: 1,
-    backgroundColor: '#f9ca24',
-    emoji: '🚗',
-    label: activityTypeLabel.Transport,
-    name: activityTypeName.transport,
-  },
-  {
-    activityTypeId: 2,
-    backgroundColor: '#f0932b',
-    emoji: '💻',
-    label: activityTypeLabel.Numerique,
-    name: activityTypeName.numeric,
-  },
-  {
-    activityTypeId: 3,
-    backgroundColor: '#eb4d4b',
-    emoji: '🍕',
-    label: activityTypeLabel.Alimentation,
-    name: activityTypeName.food,
-  },
-  {
-    activityTypeId: 4,
-    backgroundColor: '#6ab04c',
-    emoji: '⚡',
-    label: activityTypeLabel.Energie,
-    name: activityTypeName.energy,
-  },
-  {
-    activityTypeId: 5,
-    backgroundColor: '#7ed6df',
-    emoji: '🚿',
-    label: activityTypeLabel.Electromenager,
-    name: activityTypeName.appliance,
-  },
-  {
-    activityTypeId: 6,
-    backgroundColor: '#686de0',
-    emoji: '🤷‍♂️',
-    label: activityTypeLabel.Autre,
-    name: activityTypeName.other,
-  },
-]
 
 interface IactivityType {
   activityTypeId: number
